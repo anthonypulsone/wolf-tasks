6a05268dacb35d408ece6742211e4de0dd3a2c04
ajpulson <ajpulsone@ncsu.edu>
2021-04-14 22:57:57 -0400

    [Implementation] Notebook reader and test

--- Project2/src/edu/ncsu/csc216/wolf_tasks/model/io/NotebookReader.java
+++ Project2/src/edu/ncsu/csc216/wolf_tasks/model/io/NotebookReader.java
@@ -1,8 +1,18 @@
 package edu.ncsu.csc216.wolf_tasks.model.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 
 import edu.ncsu.csc216.wolf_tasks.model.notebook.Notebook;
+import edu.ncsu.csc216.wolf_tasks.model.tasks.AbstractTaskList;
+import edu.ncsu.csc216.wolf_tasks.model.tasks.Task;
+import edu.ncsu.csc216.wolf_tasks.model.tasks.TaskList;
+
 
 /**
  * NotebookReader class reads in Notebook file
@@ -20,6 +30,117 @@ public class NotebookReader {
 	 * @return a Notebook object containing the TaskGroups and tasks from the file
 	 */
 	public static Notebook readNodebookFile(File file) {
-		return null;
+		Scanner fileReader;
+	
+		try {
+			fileReader = new Scanner(new FileInputStream(file));
+		} catch (FileNotFoundException e) {
+			throw new IllegalArgumentException("Unable to load file.");
+		}
+		String firstLine = fileReader.nextLine();
+		if(firstLine.charAt(0) != '!'){
+			throw new IllegalArgumentException("Unable to load file.");
+		}
+		Notebook notebook = null;
+		String notebookName = firstLine.substring(2).trim();
+		try {
+			notebook = new Notebook(notebookName);
+		} catch (IllegalArgumentException e) {
+			throw new IllegalArgumentException("Unable to load file.");
+		}
+		
+		fileReader.useDelimiter("\\r?\\n?[#]");
+		
+		while (fileReader.hasNext()) {
+			try {
+				String taskListString = fileReader.next();
+				System.out.println(taskListString);
+				System.out.println();
+				notebook.addTaskList(processTaskList(taskListString));
+			} catch (IllegalArgumentException | NoSuchElementException e) {
+				fileReader.close();
+				throw new IllegalArgumentException("Unable to load file.");
+			}
+		}
+		fileReader.close();
+		return notebook;
+	}
+	
+	private static TaskList processTaskList(String taskListString) {
+		Scanner taskListScanner = new Scanner(taskListString);
+		String firstLine = "";
+		if (taskListScanner.hasNext()) {
+			firstLine = taskListScanner.nextLine();
+		}
+		
+		Scanner firstLineScanner = new Scanner(firstLine);
+		firstLineScanner.useDelimiter(",");
+		String taskListName = firstLineScanner.next().trim();
+		int completedCount = firstLineScanner.nextInt();
+		firstLineScanner.close();
+		
+		TaskList taskList = new TaskList(taskListName, completedCount);
+		
+		taskListScanner.useDelimiter("\\r?\\n?[*]");
+		while (taskListScanner.hasNext()) {
+			taskList.addTask(processTask(taskList, taskListScanner.next()));
+		}
+		taskListScanner.close();
+		return taskList;
+	}
+	
+	private static Task processTask(AbstractTaskList taskList, String taskString) {
+		Scanner taskScanner = new Scanner(taskString);
+		Scanner taskInfo = new Scanner(taskScanner.nextLine().trim());
+		taskInfo.useDelimiter(",");
+		Task task = null;
+		try {
+			String taskName = taskInfo.next().trim();
+			String token2 = "";
+			String token3 = "";
+			boolean recurring = false;
+			boolean active = false;
+			if (taskInfo.hasNext()) {
+				token2 = taskInfo.next().trim().toLowerCase();
+				if ("recurring".equals(token2)) {
+					recurring = true;
+				} else if ("active".equals(token2)) {
+					active = true;
+				} else {
+					taskScanner.close();
+					throw new IllegalArgumentException();
+				}
+			}
+			if (taskInfo.hasNext()) {
+				token3 = taskInfo.next().trim().toLowerCase();
+				if ("recurring".equals(token3) && recurring) {
+					taskScanner.close();
+					throw new IllegalArgumentException();
+				} else if ("recurring".equals(token3)) {
+					recurring = true;
+				} else if ("active".equals(token3) && active) {
+					taskScanner.close();
+					throw new IllegalArgumentException();
+				} else if ("active".equals(token3)) {
+					active = true;
+				} else {
+					taskScanner.close();
+					throw new IllegalArgumentException();
+				}
+			}
+			String taskDescription = "";
+			while (taskScanner.hasNext()) {
+				taskDescription += taskScanner.nextLine() + "/n";
+			}
+			task = new Task(taskName, taskDescription, recurring, active);
+			task.addTaskList(taskList);
+			taskScanner.close();
+			taskInfo.close();
+			return task;
+		} catch (InputMismatchException e) {
+			throw new IllegalArgumentException();
+			
+		
+		}
 	}
 }
--- Project2/test/edu/ncsu/csc216/wolf_tasks/model/io/NotebookReaderTest.java
+++ Project2/test/edu/ncsu/csc216/wolf_tasks/model/io/NotebookReaderTest.java
@@ -5,7 +5,11 @@ package edu.ncsu.csc216.wolf_tasks.model.io;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+
 import org.junit.Test;
+
+import edu.ncsu.csc216.wolf_tasks.model.notebook.Notebook;
 
 /**
  * Test class for Notebook Reader
@@ -20,7 +24,22 @@ public class NotebookReaderTest {
 	 */
 	@Test
 	public void testReadNodebookFile() {
-		fail("Not yet implemented");
+		Notebook n = NotebookReader.readNodebookFile(new File("test-files/notebook1.txt"));
+		
+		assertEquals("School", n.getNotebookName());
+		assertEquals(4, n.getTaskListsNames().length);
+		
+		n.setCurrentTaskList("CSC 216");
+		
+		assertEquals("CSC 216", n.getCurrentTaskList().getTaskListName());
+		
+		n.setCurrentTaskList("CSC 226");
+		
+		assertEquals("CSC 226", n.getCurrentTaskList().getTaskListName());
+		
+		n.setCurrentTaskList("CSC 226");
+		
+		assertEquals("CSC 226", n.getCurrentTaskList().getTaskListName());
 	}
 
 }
